c8ed05e16f66dfcbfe099b9242eaa6c7d30b3136
Rafael Moraes <rafaelsm003@gmail.com>
2023-03-28 14:40:11 -0300

    feat: implementa a classe Report

---
+++ src/main/java/com/trybe/acc/java/controledeacesso/Report.java
@@ -0,0 +1,12 @@
+package com.trybe.acc.java.controledeacesso;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+/**
+ * Classe Report.
+ */
+public class Report {
+  
+}
